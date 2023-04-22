33760b06187e0b80593a930693dce42c4793cab4
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-04-22 12:05:01 +0300

    INITIAL COMMIT

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Price_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
