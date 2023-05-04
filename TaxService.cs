c42d245fad4038f44a3f4d00571ddcb834ca6716
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-05-04 12:08:23 +0300

    Feature: Precedence Feature is built

--- TaxService.cs
+++ TaxService.cs
@@ -1,13 +1,24 @@
-﻿namespace PriceCalculator
+﻿using Price_Calculator;
+
+namespace PriceCalculator
 {
     public class TaxService : ITax
 
     {
+        public double CalculateDiscountAfterTax(Product product, Discount discount, Tax tax)
+        {
+            var priceAfterDiscount =product.price.RegularPrice- product.price.RegularPrice * discount.DiscountRate;
+           
+            var TotalDiscountAmount = priceAfterDiscount * tax.TaxRate;
+            return Math.Round(TotalDiscountAmount, 2); 
+        }
+
         public double CalculateTax(Product product, Tax tax)
         {
             var taxAmoount = product.price.RegularPrice * tax.TaxRate;
             return Math.Round(taxAmoount, 2);
         }
+
 
     }
 }
