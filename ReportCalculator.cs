a078dcae9288cbfa82bf1f4b91e8db3f7a461689
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-05-04 16:30:55 +0300

    Feather: Combining Featuer has been added
    Using a user Defind type Called DiscountType
    If the Discounttype is multiplicative a new edit for the way of calculating total discoint is being done

--- ReportCalculator.cs
+++ ReportCalculator.cs
@@ -6,34 +6,45 @@
         public Discount discount { get; set; }
         public UPCDiscount upcDiscount { get; set; }
         internal Price OrginialPrice { get; set; }
+        public DiscountType discountType { get; set; }
         public List<AdditionalCost> additionalCosts { get; set;}
         internal Price TotalPriceToPrint {  get; private set; }
         internal double TaxAmount { get; set; }
         internal double DiscountAmount { get; set; }
         public void SetupReport(Product product)
         {
+            discountType = DiscountType.MultiplicativDiscount;
             TaxAmount = 0;
             DiscountAmount = 0;
             OrginialPrice = product.price;
-            bool discountIsNotSet = true;
-            bool upcDiscountIsNotSet = true;
             DiscountService discountService = new DiscountService();
+            
             if (discount.IsBeforeTax)
             {
                 DiscountAmount += discountService.CalculateDiscount(product, discount);
-                discountIsNotSet = false;
+              
             }
             if (upcDiscount.Discount.IsBeforeTax)
             {
+                if (discountType.Equals(DiscountType.MultiplicativDiscount))
+                {
+                    product.price = new(OrginialPrice.RegularPrice - DiscountAmount);
+                }
                 DiscountAmount += discountService.CalculateDiscount(product, upcDiscount.Discount);
-                upcDiscountIsNotSet = false;
             }
             product.price = new (OrginialPrice.RegularPrice - DiscountAmount);
 
             setTaxAmount(product);
 
-            if (discountIsNotSet) DiscountAmount += discountService.CalculateDiscount(product, discount);
-            if (upcDiscountIsNotSet) DiscountAmount += discountService.CalculateDiscount(product, upcDiscount.Discount);
+            if (!discount.IsBeforeTax) DiscountAmount += discountService.CalculateDiscount(product, discount);
+            if (!upcDiscount.Discount.IsBeforeTax) {
+                if (discountType.Equals(DiscountType.MultiplicativDiscount))
+                {
+                    product.price = new(OrginialPrice.RegularPrice - DiscountAmount);
+                }
+            }
+            DiscountAmount += discountService.CalculateDiscount(product, upcDiscount.Discount);
+
             DiscountAmount=Math.Round(DiscountAmount, 2);
             GetTotalPrice();
 
@@ -55,6 +66,7 @@
             OrginialPrice=new Price();
             additionalCosts = new List<AdditionalCost>();
             TotalPriceToPrint=new Price();
+            discountType= new DiscountType();
         }
         private void AdjustCost() {
 
