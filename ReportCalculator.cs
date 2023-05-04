0c861091d5194db0722d1c18383e1d87bf92889a
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-05-04 13:20:17 +0300

    UPDATE:
    New speration for Report Has been added
    New class called for ReportCalculator is being introduced and it's job to handle the
    calculations, and the ReportPrinter it's responsible for printing the report only.

---
+++ ReportCalculator.cs
@@ -0,0 +1,66 @@
+﻿namespace PriceCalculator
+{
+    public class ReportCalculator
+    {
+        public Tax tax { get; set; }
+        public Discount discount { get; set; }
+        public UPCDiscount upcDiscount { get; set; }
+        internal Price OrginialPrice { get; set; }
+        public List<AdditionalCost> additionalCost { get; set;}
+        internal Price TotalPriceToPrint {  get; private set; }
+        internal double TaxAmount { get; set; }
+        internal double DiscountAmount { get; set; }
+        public void SetupReport(Product product)
+        {
+            TaxAmount = 0;
+            DiscountAmount = 0;
+            OrginialPrice = product.price;
+            bool discountIsNotSet = true;
+            bool upcDiscountIsNotSet = true;
+            DiscountService discountService = new DiscountService();
+            if (discount.IsBeforeTax)
+            {
+                DiscountAmount += discountService.CalculateDiscount(product, discount);
+                discountIsNotSet = false;
+            }
+            if (upcDiscount.Discount.IsBeforeTax)
+            {
+                DiscountAmount += discountService.CalculateDiscount(product, upcDiscount.Discount);
+                upcDiscountIsNotSet = false;
+            }
+            product.price = new(OrginialPrice.RegularPrice - DiscountAmount);
+
+            setTaxAmount(product);
+            if (discountIsNotSet) DiscountAmount += discountService.CalculateDiscount(product, discount);
+            if (upcDiscountIsNotSet) DiscountAmount += discountService.CalculateDiscount(product, upcDiscount.Discount);
+
+        }
+        public void setTaxAmount(Product product)
+        {
+            TaxAmount = 0;
+            TaxService taxService = new TaxService();
+
+            TaxAmount = taxService.CalculateTax(product, tax);
+
+        }
+        public ReportCalculator()
+        {
+            tax = new Tax();
+            discount = new Discount();
+            upcDiscount=new UPCDiscount();
+            OrginialPrice=new Price();
+            additionalCost = new List<AdditionalCost>();
+            TotalPriceToPrint=new Price();
+        }
+
+        public Price GetTotalPrice() {
+            double value = this.TaxAmount + this.OrginialPrice.RegularPrice - this.DiscountAmount;
+            TotalPriceToPrint = new Price(value);
+            return TotalPriceToPrint;
+        }
+
+
+
+
+    }
+}
---
+++ ReportPrinter.cs
@@ -0,0 +1,49 @@
+﻿namespace PriceCalculator
+{
+    public class ReportPrinter
+    {
+        public ReportCalculator ReportCalculator { get; set; }
+        bool ReportIsNotSet { get; set; }
+        public ReportPrinter()
+        {
+            ReportCalculator = new ReportCalculator();
+            ReportIsNotSet = true;
+        }
+        public ReportPrinter(ReportCalculator reportCalculator)
+        {
+            this.ReportCalculator = reportCalculator;
+            ReportIsNotSet = true;
+
+        }
+        public void SimplePrint(Product product)
+        {
+            var TotalOriginalPrice = product.price.FormattedPrice;
+            if (ReportIsNotSet)
+            {
+                ReportCalculator.SetupReport(product);
+                ReportIsNotSet = false;
+            }
+            Console.WriteLine($"Title \"{product.Name}\", UPC = {product.Upc}, price= {TotalOriginalPrice}");
+
+        }
+        public void PrintWithTotalDiscount(Product product)
+        {
+            if (ReportIsNotSet)
+            {
+                ReportCalculator.SetupReport(product);
+                ReportIsNotSet = false;
+            }
+
+            var DiscountAmount = ReportCalculator.DiscountAmount;
+            Console.WriteLine($"Total Discount Amount is {DiscountAmount}");
+
+        }
+        public void PrintTotalPrice()
+        {
+            Console.WriteLine($"Total Price With All Discounts is {ReportCalculator.TotalPriceToPrint.FormattedPrice}");
+            Console.WriteLine($"Total Discoint is {ReportCalculator.DiscountAmount}\n");
+            ReportIsNotSet = true;
+        }
+
+    }
+}
